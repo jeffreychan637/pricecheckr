803d23c1758d748505e2adf70a918e8667cf544a
Jeffrey Chan <jeffreychan637@berkeley.edu>
2017-03-25 02:01:13 -0700

    added Go file; setup functions and formatting for program

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "os/exec"
+)
+
+func runCasperJsScript() {
+    //start process
+    //get recorded data
+    //wait for process
+    //compare data
+}
+
+func getRecordedData() {
+
+}
+
+func compareData() {
+    //compare and decide if email needs to be sent out
+}
+
+func buildEmail() {
+
+}
+
+func sendEmail() {
+
+}
+
+func main() {
+    runCasperJsScript()
+
+}
